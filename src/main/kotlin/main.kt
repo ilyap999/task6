a804d6437b882b40d47d1b6a1af4f823f0c76679
Ilya Permyakov <ilyap999@mail.ru>
2021-08-21 10:52:15 +0400

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,122 @@
+fun main() {
+
+}
+
+class WallService {
+    private var posts = emptyArray<Post>()
+    private var id = 1
+
+    fun add(post: Post): Post {
+        post.id = id
+        id++
+        posts += post
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, postInArray) in posts.withIndex()) {
+            if (post.id == postInArray.id) {
+                posts[index] = post.copy(ownerId = postInArray.ownerId, date = postInArray.date)
+                return true
+            }
+        }
+        return false
+    }
+}
+
+data class Post(
+    var id: Int,
+    val ownerId: Int,
+    val fromId: Int,
+    val createdBy: Int,
+    val date: Int,
+    val text: String,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
+    val friendsOnly: Boolean,
+    val comments: Comments,
+    val copyright: Copyright?,
+    val likes: Likes,
+    val reposts: Reposts,
+    val views: Views,
+    val postType: String,
+    val signerId: Int,
+    val canPin: Boolean,
+    val canDelete: Boolean,
+    val canEdit: Boolean,
+    val isPinned: Boolean,
+    val markedAsAds: Boolean,
+    val isFavorite: Boolean,
+    val donut: Donut,
+    val postponedId: Int,
+    val postSource: PostSource?,
+    val geo: Geo?,
+    val copyHistory: Array<Post>?
+)
+
+data class Comments(
+    val count: Int,
+    val canPost: Boolean,
+    val groupsCanPost: Boolean,
+    val canClose: Boolean,
+    val canOpen: Boolean
+)
+
+data class Copyright(
+    val id: Int,
+    val link: String,
+    val name: String,
+    val type: String
+)
+
+data class Likes(
+    val count: Int,
+    val userLikes: Boolean,
+    val canLike: Boolean,
+    val canPublish: Boolean
+)
+
+data class Reposts(
+    val count: Int,
+    val userReposted: Boolean
+)
+
+data class Views(
+    val count: Int
+)
+
+data class Donut(
+    val isDonut: Boolean,
+    val paidDuration: Int,
+    val placeholder: String,
+    val canPublishFreeCopy: Boolean,
+    val editMode: String
+)
+
+data class PostSource(
+    val type: String,
+    val platform: String,
+    val data: String,
+    val url: String
+)
+
+data class Place(
+    val id: Int,
+    val title: String,
+    val latitude: Int,
+    val longitude: Int,
+    val created: Int,
+    val icon: String,
+    val chekins: Int,
+    val updated: Int,
+    val type: Int,
+    val country: Int,
+    val city: Int,
+    val address: String
+)
+
+data class Geo(
+    val type: String,
+    val coordinates: String,
+    val place: Place?
+)
